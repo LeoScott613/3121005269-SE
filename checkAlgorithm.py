00ec958ca254afde786591969ab4ec49ff0ee3e9
Leoscott <PRCSteven06@gmail.com>
2023-09-14 16:08:23 +0800

    -install sentence transformer -use sentence transformer to analyse duplicate degree -add .gitignore -delete irrelevant NLTK -successfully calculate text similarity

--- checkAlgorithm.py
+++ checkAlgorithm.py
@@ -1,8 +1,12 @@
-import nltk
-# nltk.download('punkt') should run it once
-# try to tokenize a sentence
-word_data="The great mathematician David Hilbert noticed that a number of important mathematical arguments were structurally similar."
-nlp_token=nltk.word_tokenize(word_data)
-unordered=list(set(nlp_token))
+from sentence_transformers.util import cos_sim  
+from sentence_transformers import SentenceTransformer as SBert
 
-print(unordered)
+st_model=SBert("./paraphrase-multilingual-MiniLM-L12-v2")
+cn_sentence1=['今天吃吮指原味鸡']
+cn_sentence2=['今天我吃了吮指原味鸡']
+
+embeddings1=st_model.encode(cn_sentence1)
+embeddings2=st_model.encode(cn_sentence2)
+cosine_score=cos_sim(embeddings1,embeddings2)
+
+print ("文本相似度",cosine_score.item()*100,"%")
